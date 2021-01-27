cc8d38e83e11049ed9773b4c849662033ad087ee
AkashM398 <akashm1999@gmail.com>
2021-01-27 15:22:40 +0530

    Modified env variables

--- next.config.js
+++ next.config.js
@@ -9,9 +9,9 @@ module.exports = {
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
     AUTH0_SCOPE: 'openid profile offline_access',
     REDIRECT_URI:
-      process.env.REDIRECT_URI || 'http://localhost:3000/api/callback',
+      process.env.REDIRECT_URI || 'https://skcript-microblog.vercel.app/api/callback',
     POST_LOGOUT_REDIRECT_URI:
-      process.env.POST_LOGOUT_REDIRECT_URI || 'http://localhost:3000/',
+      process.env.POST_LOGOUT_REDIRECT_URI || 'https://skcript-microblog.vercel.app/',
     SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
     SESSION_COOKIE_LIFETIME: 7200 // 2 hours
   }
