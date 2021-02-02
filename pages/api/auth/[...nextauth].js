88305b9b6fe2e9c6fa914b4bf57aec6a50b1a04c
AkashM398 <akashm1999@gmail.com>
2021-02-02 10:05:05 +0530

    Changed Domain in cookie

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -47,7 +47,7 @@ const options = {
         sameSite: 'lax',
         path: '/',
         secure: useSecureCookies,
-        domain: hostName == 'makemicroblog.ml' ? hostName : '.' + hostName // add a . in front so that subdomains are included
+        domain: auth.makemicroblog.ml,
       }
     },
   },  
